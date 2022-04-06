4023bf0e735d661b91d32c825ccea6d1f8784a08
Alex <aa@bam-boo.eu>
2022-04-06 22:00:47 +0300

    fix(core): fix list of video outputs

--- src/core/vlc.cpp
+++ src/core/vlc.cpp
@@ -1,21 +1,21 @@
 /******************************************************************************
-* This file is part of QuickVLC - Qt and libvlc connection library.
-* Copyright (C) 2016 Tadej Novak <tadej@tano.si>, 
-*               2022 Alexey Avramchik (OU Bamboo group) <aa@bam-boo.eu>
-*
-* QuickVLC is free software: you can redistribute it and/or modify it 
-* under the terms of the GNU General Public License as published 
-* by the Free Software Foundation, either version 3 of the License, 
-* or (at your option) any later version.
-*
-* QuickVLC is distributed in the hope that it will be useful, 
-* but WITHOUT ANY WARRANTY; without even the implied warranty 
-* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-* See the GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License 
-* along with QuickVLC. If not, see <https://www.gnu.org/licenses/>.
-******************************************************************************/
+ * This file is part of QuickVLC - Qt and libvlc connection library.
+ * Copyright (C) 2016 Tadej Novak <tadej@tano.si>,
+ *               2022 Alexey Avramchik (OU Bamboo group) <aa@bam-boo.eu>
+ *
+ * QuickVLC is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * QuickVLC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QuickVLC. If not, see <https://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 #include "vlc.h"
 
@@ -218,12 +218,12 @@ QStringList Enum::videoOutput()
 #if defined(Q_OS_LINUX)
     list << "x11"
          << "xvideo"
-         << "glx"
+         << "glx";
 #elif defined(Q_OS_WIN32)
     list << "directx"
-         << "direct3d"
+         << "direct3d";
 #endif
-         << "opengl";
+    list << "opengl";
 
     return list;
 }
